16671d453148069ef91286f88f3bc3774c10f8f5
Jaden Sutton <jadensutton_3@hotmail.com>
2022-12-26 23:18:54 -0500

    Added heat map feature and cleaned up code

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import flask
-import json
 import requests
 
 import numpy as np
 
 from scipy.stats import norm
+from math import ceil, log10, floor
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-from flask import request
+from flask import request, jsonify
 from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 
@@ -22,10 +22,32 @@ def scrape_spot_price(ticker) -> float:
     price = float(soup.find('fin-streamer', {'class' : 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).get('value'))
     return price
 
-def black_scholes(type: str, spot: float, strike: float, risk_free_rate: float, time_to_exp: float, vol: float) -> float:
+def heatmap(type: str, spot: float, strike: float, risk_free_rate: float, exp_date: datetime, imp_vol: float) -> list:
+    #Fill out arrays for the x and y ranges
+    now = datetime(*datetime.now().timetuple()[:3])
+    days_to_exp = ceil((exp_date - now).total_seconds() / 86400)
+    if days_to_exp >= 365:
+        x_labels = [now + timedelta(days=30*x) for x in range(1, floor(days_to_exp / 30))]
+    elif days_to_exp >= 60:
+        x_labels = [now + timedelta(days=7*x) for x in range(1, floor(days_to_exp / 7))]
+    elif days_to_exp > 0:
+        x_labels = [now + timedelta(days=x) for x in range(1, days_to_exp)]
+    else:
+        raise Exception('Expiry date must be greater than current date')
+
+    x_range_imp_vol = imp_vol / np.sqrt(365 / days_to_exp)
+    y_precision = 10 ** ceil(log10(spot * 0.001))
+    y_labels = [x for x in range(round(spot * (1 - x_range_imp_vol) / y_precision) * y_precision, round(spot * (1 + x_range_imp_vol) / y_precision) * y_precision + 1, y_precision)]
+
+    #Calculate the black scholes value for every date and underlying price combination
+    data = [{'key': date.strftime('%m/%d/%Y'), 'data': [{'key': price, 'data': black_scholes(type, price, strike, risk_free_rate, (exp_date - date).total_seconds() / (86400 * 365), imp_vol)} for price in y_labels]} for date in x_labels]
+
+    return data
+
+def black_scholes(type: str, spot: float, strike: float, risk_free_rate: float, time_to_exp: float, imp_vol: float) -> float:
     #Calculate d1 and d2
-    d1 = (np.log(spot / strike) + (risk_free_rate + vol ** 2 / 2) * time_to_exp) / (vol * np.sqrt(time_to_exp))
-    d2 = d1 - vol * np.sqrt(time_to_exp)
+    d1 = (np.log(spot / strike) + (risk_free_rate + imp_vol ** 2 / 2) * time_to_exp) / (imp_vol * np.sqrt(time_to_exp))
+    d2 = d1 - imp_vol * np.sqrt(time_to_exp)
 
     #Apply the call or put formula to calculate value
     if type == 'C':
@@ -45,17 +67,18 @@ def price_option() -> dict:
         type = r['type']
         strike = float(r['strike'])
         risk_free_rate = float(r['risk_free_rate']) / 100
-        vol = float(r['vol']) / 100
+        imp_vol = float(r['imp_vol']) / 100
         exp_date = datetime.strptime(r['exp_date'], '%Y-%m-%d') + timedelta(minutes=990)
+        include_heatmap = r['include_heatmap'] == 'true'
         now = datetime.now()
         time_to_exp = (exp_date - now).total_seconds() / (86400 * 365)
         spot = scrape_spot_price(ticker)    #Get spot price
 
-        return {'status': 'success', 'result': black_scholes(type, spot, strike, risk_free_rate, time_to_exp, vol)}
+        return jsonify(value=black_scholes(type, spot, strike, risk_free_rate, time_to_exp, imp_vol), heatmap_data=heatmap(type, spot, strike, risk_free_rate, exp_date, imp_vol) if include_heatmap else []), 200
 
     except Exception as e:
         print(e)
-        return {'status': 'failure', 'result': 'Error encountered. Please ensure all the inputs are valid.'}
+        return 'Error encountered. Please ensure all the inputs are valid.', 400
 
 @app.route('/')
 @cross_origin()
--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,49 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState } from 'react'
 
 import { Form, Button, Card, Alert, Navbar, Container, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Heatmap, HeatmapSeries } from 'reaviz'
 import axios from 'axios'
 
 function App() {
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
-    const [data, setData] = useState('NaN')
+    const [option_value, setOptionValue] = useState('')
+    const [heatmap_data, setHeatmapData] = useState([])
 
     const ticker = useRef()
     const type = useRef()
     const strike = useRef()
     const riskFreeRate = useRef()
     const expDate = useRef()
-    const vol = useRef()
+    const impVol = useRef()
+    const includeHeatmap = useRef()
+
+    const getTestDate = () => {
+        const testDate = new Date()
+        testDate.setDate(testDate.getDate() + 90)
+        var month = testDate.getMonth() + 1
+        if (month <= 9) {
+            month = '0' + month
+        }
+        var day = testDate.getDate()
+        if (day <= 9) {
+            day = '0' + day
+        }
+
+        return `${testDate.getFullYear()}-${month}-${day}`
+    }
+
+    function quickTest(e) {
+        ticker.current.value = 'AAPL'
+        type.current.value = 'C'
+        strike.current.value = 150
+        riskFreeRate.current.value = 0
+        expDate.current.value = getTestDate()
+        impVol.current.value = 25
+        includeHeatmap.current.value = 'true'
+
+        handleSubmit(e)
+    }
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -21,24 +51,21 @@ function App() {
         setError('')
         setLoading(true)
         
-
-        await axios.get('https://black-scholes-options-pricer.herokuapp.com/price_option', {
+        await axios.get('/price_option', {
                 params: {
                     ticker: ticker.current.value,
                     type: type.current.value,
                     strike: strike.current.value,
                     risk_free_rate: riskFreeRate.current.value,
                     exp_date: expDate.current.value,                  
-                    vol: vol.current.value
+                    imp_vol: impVol.current.value,
+                    include_heatmap: includeHeatmap.current.value
                 }
             }).then(response => {
-                if (response['data']['status'] == 'success') {
-                    setData(response['data']['result'])
-                } else {
-                    setError(response['data']['result'])
-                }
+                setOptionValue(response['data']['value'])
+                setHeatmapData(response['data']['heatmap_data'])
             }).catch(error => {
-                setError('Unknown error encountered.')
+                setError('Experienced error. Please ensure all inputs are valid')
         })
 
         setLoading(false)
@@ -56,11 +83,12 @@ function App() {
             style={{minHeight: '130vh'}}
             >
                 <div className='w-100 d-flex align-items-center justify-content-center'>        
-                    <div style={{minWidth: '600px'}}>
+                    <div style={{minWidth: '600px', marginBottom: '50px'}}>
                         <Card>
                             <Card.Body>
                                 <h2 className='text-center mb-4'>Option Variables</h2>
                                 {error && <Alert variant='danger'>{error}</Alert>}
+
                                 <Form onSubmit={handleSubmit}>
                                     <Form.Group id='option-variables'>
                                         <Form.Label>Underlying Ticker</Form.Label>
@@ -82,17 +110,39 @@ function App() {
                                         <Form.Control type='date' ref={expDate} required />
 
                                         <Form.Label>Implied Volatility (%)</Form.Label>
-                                        <Form.Control type='text' ref={vol} required /> 
+                                        <Form.Control type='text' ref={impVol} required /> 
+
+                                        <Form.Label>Include Heat Map</Form.Label>
+                                        <Form.Select ref={includeHeatmap} required>
+                                            <option value='true'>Yes</option>
+                                            <option value='false'>No</option>                                            
+                                        </Form.Select> 
                                     </Form.Group>
 
                                     <Button disabled={loading} className='w-100 mt-4' type='submit'>
                                         Price Option
-                                    </Button>
-
-                                    {data != 'NaN' ? <h5 className='text-center mb-4 mt-4'>Option Value: ${data}</h5> : null}                                 
+                                    </Button>                                 
                                 </Form>
+
+                                <div className='text-center'>
+                                    or
+                                </div>
+
+                                <Button disabled={loading} className='w-100' onClick={quickTest}>
+                                    Quick Test
+                                </Button>
+
+                                {option_value ? <h5 className='text-center mb-4 mt-4'>Option Value: ${option_value}</h5> : null}
                             </Card.Body>
                         </Card>
+                        
+                        {heatmap_data.length ? <h5 className='text-center mb-4 mt-4'>Heat Map</h5> : null}
+                        <Heatmap
+                            height={250}
+                            width={'100%'}
+                            data={heatmap_data}
+                            series={<HeatmapSeries colorScheme={['#cce7ff', '#99cfff', '#66b8ff', '#33a0ff', '#0088ff', '#0074d9']} />}
+                        />
                     </div>
                 </div>
             </div>   
